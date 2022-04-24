2eaa0e2d8667121411a65b55eac7edcfaed26f12
kamal-kishor-rajput <90085564+kamal-kishor-rajput@users.noreply.github.com>
2022-04-25 00:31:11 +0530

    timer assignment completed

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <Timer />
+      <Timer timer={{ starttimer: 3, endtimer: 20 }} />
     </div>
   );
 }
--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,9 +1,28 @@
-export const Timer = () => {
-    
-    return (
+import { useState, useEffect } from "react";
+export const Timer = ({ timer }) => {
+  const [starttime, setstarttime] = useState(timer.starttimer);
+  const endtime = timer.endtimer;
+  const [state, setstate] = useState(false);
+  useEffect(() => {
+    let id = setTimeout(() => {
+      setstarttime(starttime + 1);
+    }, 1000);
+    if (starttime === endtime) {
+      clearInterval(id);
+      setstate(true);
+    }
 
-        <div>
-            timers.
-        </div>
-    )
-}
+    setstarttime(starttime);
+  }, [starttime, endtime]);
+  //   useEffect(() => {
+  //     console.log("calling useeffect endtimer");
+  //     setendtime(endtime);
+  //   }, [endtime]);
+  return (
+    <div>
+      <div>Start Timer:{starttime}</div>
+      <div>End Timer:{endtime}</div>
+      <h2>{state ? "Time Up" : "time is running"}</h2>
+    </div>
+  );
+};
